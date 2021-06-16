5e21f4fc9db01ee93b90a14224970b95842fabc6
kiprono <kiprono_emmanuel@yahoo.com>
2021-06-16 15:06:05 +0300

    one to one

---
+++ one_to_one.py
@@ -0,0 +1,17 @@
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///121rel.db'
+
+db = SQLAlchemy(app)
+
+class Parent(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(500))
+    child = db.relationship('Child', backref='parent', uselist=False)
+
+class Child(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(500))
+    parent_id = db.Column(db.Integer, db.ForeignKey('parent.id'))
